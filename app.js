f6a8367bfd30cedc26b146c4872d2778e69d9717
Aki Sharipov <aki@multiaki.com>
2013-07-15 05:25:02 -0400

    added about us page

--- app.js
+++ app.js
@@ -28,6 +28,8 @@ app.configure('development', function(){
 });
 
 app.get('/', routes.index);
+app.get('/about', routes.about);
+
 app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
