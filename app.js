8104683a1a1910b9ba3d6820a1fc76c1e1b0f346
Aki Sharipov <aki@multiaki.com>
2013-07-16 11:10:04 -0400

    fixed app.js echo text
    shows amazon url

--- app.js
+++ app.js
@@ -33,5 +33,5 @@ app.get('/about', about.about);
 app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
-  console.log("Express server listening on port " + app.get('port'));
+  console.log("http://ec2-174-129-66-87.compute-1.amazonaws.com:8080 Express server listening on port " + app.get('port'));
 });
